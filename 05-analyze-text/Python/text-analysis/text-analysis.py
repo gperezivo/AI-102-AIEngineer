0f48262574238ac5462ecdce13f4444e492beb49
gperezivo <gperezi@vernegroup.com>
2021-12-14 21:09:12 +0100

    feat: text-analysis detect language and sentiment (python)

--- 05-analyze-text/Python/text-analysis/text-analysis.py
+++ 05-analyze-text/Python/text-analysis/text-analysis.py
@@ -1,3 +1,5 @@
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 from dotenv import load_dotenv
 import os
 
@@ -13,7 +15,8 @@ def main():
 
         # Create client using endpoint and key
 
-
+        credential = AzureKeyCredential(cog_key)
+        client = TextAnalyticsClient(endpoint=cog_endpoint, credential=credential)
         # Analyze each text file in the reviews folder
         reviews_folder = 'reviews'
         for file_name in os.listdir(reviews_folder):
@@ -23,10 +26,18 @@ def main():
             print('\n' + text)
 
             # Get language
+            documents = [text]
+            result = client.detect_language(documents)
+            
+            successful_responses = [doc for doc in result if not doc.is_error]
+            
+            print(successful_responses)
 
 
             # Get sentiment
-
+            lang = successful_responses[0].primary_language
+            result = client.analyze_sentiment(documents)
+            print(result)
 
             # Get key phrases
 
